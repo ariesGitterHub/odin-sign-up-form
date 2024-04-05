56a4d3feb6d1586ed8fdcc56964a4214a1e3c5a1
ariesGitterHub <149708069+ariesGitterHub@users.noreply.github.com>
2024-04-05 12:51:03 -0400

    Tweaked popup removed onclick from HTML, more font tweaking

--- javascript.js
+++ javascript.js
@@ -1,9 +1,17 @@
 // When the user clicks on <div>, open the popup
 
+// function myFunction() {
+//   const popup = document.getElementById("myPopup");
+//   popup.classList.toggle("show");
+// }
 
+const popUp = document.querySelector(".pop-up");
 
+// Note, I was getting the error: Cannot read properties of null (reading 'addEventListener'). Adding "defer" to the html head here: "<script src="javascript.js" defer></script>" fixed this null issue. KEEP THIS COMMENT FOR FUTURE REFERENCE.
 
-function myFunction() {
-  const popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
+popUp.addEventListener("click", myPopUpFunc);
+const popUpSpan = document.querySelector("#my-pop-up");
+
+function myPopUpFunc() {
+  popUpSpan.classList.toggle("pop-up-show");
 }
