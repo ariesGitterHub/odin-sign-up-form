704592fe9026995eb0d0d8452a37cae90ea4816b
ariesGitterHub <149708069+ariesGitterHub@users.noreply.github.com>
2024-04-09 10:02:42 -0400

    Solved problem of form submission without password/confirm password matching

--- javascript.js
+++ javascript.js
@@ -35,22 +35,37 @@ function showPassword() {
   }
 }
 
-//  Wrong password
+//  Wrong password 
 
-password.addEventListener("blur", passwordChecker);
-confirmPassword.addEventListener("blur", passwordChecker);
+password.addEventListener("blur", checkPasswordCreateMsg);
+confirmPassword.addEventListener("blur", checkPasswordCreateMsg);
 
-function passwordChecker() {
-  const passwordCheckedText = document
-  .querySelector("#wrong-password"); 
+function checkPasswordCreateMsg() {
+  const passwordCheckedMsg = document.querySelector("#password-check-msg"); 
 
-  if (password.value !== confirmPassword.value) {
-  passwordCheckedText.innerText = "Passwords do not match!";
-  } else if (password.value === confirmPassword.value) {
-   passwordCheckedText.innerText = "Both passwords match.";
-  } else if (password.value === "" && confirmPassword.value === "") {
-  passwordCheckedText.innerText = "";
+  if (password.value === confirmPassword.value) {
+    passwordCheckedMsg.innerText = "Both passwords match.";
+    confirmPassword.style.cssText = "border: 2px solid var(--my-white)";
+  }
+  else if (password.value !== confirmPassword.value) {
+    passwordCheckedMsg.innerText = "Passwords do not match!";
+    confirmPassword.style.cssText = "border: 5px double var(--my-red)";
+     } 
+  else if (password.value === "" || confirmPassword.value === "") {
+    passwordCheckedText.innerText = "";
+    confirmPassword.style.cssText = "border: 5px double var(--my-red)";
   }
 }
 
+// Stop submit if password fields are not identical
 
+const formSubmit = document.querySelector("form");
+formSubmit.addEventListener("submit", submitChecker);
+
+function submitChecker(event) {
+  if (password.value !== confirmPassword.value || password.value === "" || confirmPassword.value === "") {
+    event.preventDefault();
+  } else {
+    return;
+  }
+}
